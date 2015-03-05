acfe3c58ab9c2aa34e68740bc3c7dc37edeca6cd
iheartcakie <abonnet@kippnyccp.org>
2015-03-05 13:18:10 -0500

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,5 +1,71 @@
-$(document).ready(function(){
-  for(i=1; i<21; i++){
-    $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
-  }
+var Tile = function(x, y, face) {
+    this.x = x;
+    this.y = y;
+    this.face = face;
+    this.width = 70;
+};
+
+Tile.prototype.drawFaceDown = function() {
+    fill(214, 247, 202);
+    rect(this.x, this.y, this.width, this.width, 10);
+    image(getImage("avatars/leaf-green"), this.x, this.y, this.width, this.width);
+};
+
+Tile.prototype.drawFaceUp = function() {
+    fill(214, 247, 202);
+    strokeWeight(2);
+    rect(this.x, this.y, this.width, this.width, 10);
+    image(this.face, this.x, this.y, this.width, this.width);
+};
+
+
+// Declare an array of all possible faces
+var faces = [
+    getImage("avatars/leafers-seed"),
+    getImage("avatars/leafers-seedling"),
+    getImage("avatars/leafers-sapling"),
+    getImage("avatars/leafers-tree"),
+    getImage("avatars/leafers-ultimate"),
+    getImage("avatars/marcimus"),
+    getImage("avatars/mr-pants"),
+    getImage("avatars/mr-pink"),
+    getImage("avatars/old-spice-man"),
+    getImage("avatars/robot_female_1"),
+    getImage("avatars/piceratops-tree"),
+    getImage("avatars/orange-juice-squid")
+];
+
+// Make an array which has 2 of each, then randomize it
+var selected = [];
+for (var i = 0; i < 10; i++) {
+    // Randomly pick one from the array of remaining faces
+    var randomInd = floor(random(faces.length));
+    var face = faces[randomInd];
+    // Push 2 copies onto array
+    selected.push(face);
+    selected.push(face);
+    // Remove from array
+    faces.splice(randomInd, 1);
+}
+
+// Now we need to randomize the array
+selected.sort(function() {
+    return 0.5 - random();
 });
+
+// Create the tiles
+var tiles = [];
+var NUM_COLS = 5;
+var NUM_ROWS = 4;
+for (var i = 0; i < NUM_COLS; i++) {
+    for (var j = 0; j < NUM_ROWS; j++) {
+        tiles.push(new Tile(i * 78 + 10, j * 78 + 40, selected.pop()));
+    }
+}
+
+background(255, 255, 255);
+
+// Now draw them face up
+for (var i = 0; i < tiles.length; i++) {
+    tiles[i].drawFaceUp();
+}
