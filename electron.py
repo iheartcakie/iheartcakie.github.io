ae326b30f8ffe1fb105d080be2986ad546ffbc78
iheartcakie <abonnet@kippnyccp.org>
2015-05-12 13:30:15 -0400

    Update electron.py

--- electron.py
+++ electron.py
@@ -1,14 +1,14 @@
-from random import randint 
-total=0
-for i in range(10000):
-  wins=0
-  if randint(1,100) <= 87:
+from random import random #importing numbers from the random function
+total=0 #setting in a variable to zero so that the total can start at zero
+for i in range(10000): # for all the numbers in the range of 10,000
+  wins=0 # keeps track of the winner in each election
+  if random(1,100) <= 87: #87 is the chance of winning in that region 
     wins=wins+1
-  if randint(1,100) <= 65:
+  if random(1,100) <= 65:
     wins=wins+1
-  if randint(1,000) <= 17:
+  if random(1,100) <= 17: 
     wins=wins+1
-  if wins>=2
+  if wins>=2: #if wins is greater than or equal to 2 then it adds it to the total variable 
     total+=1
-winning= (total/(float(10000))
-print "your chance in winning is %" chance
+winning= (total/(float(10000)) # takes the total and rounds it
+print "Your chance in winning is %" (chance)
