9ae0140614749454eef4103cc82d62fbabe4cfd7
iheartcakie <abonnet@kippnyccp.org>
2015-05-12 12:59:15 -0400

    Create electron.py

---
+++ electron.py
@@ -0,0 +1,14 @@
+from random import randint 
+total=0
+for i in range(10000):
+  wins=0
+  if randint(1,100) <= 87:
+    wins=wins+1
+  if randint(1,100) <= 65:
+    wins=wins+1
+  if randint(1,000) <= 17:
+    wins=wins+1
+  if wins>=2
+    total+=1
+winning= (total/(float(10000))
+print "your chance in winning is %" & chance
