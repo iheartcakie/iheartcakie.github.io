ca6cda257455c45339bf583306bdc78e3cf5dd26
iheartcakie <abonnet@kippnyccp.org>
2015-05-20 13:02:58 -0400

    Update basic_maze.py

--- basic_maze.py
+++ basic_maze.py
@@ -3,14 +3,34 @@ import time
 import math
 
 set_speed(100)
-right () 
-enc_tgt(0,1,14)
-enc_(1,1, floor (1219.2))
-time.sleep(.1)
-fwd()  
-time.sleep(3)
 
 def move_forward(feet):
-  mm+304.8 * feet
+  mm = 304.8 * feet
+  steps = mm/11.344
+  enc_tgt(1,1,steps)
+  time.sleep(.1)
+  fwd()
+  time.sleep(4*feet)
 
-move forward (3)
+def turn_right():
+  enc_tgt(1,0,15)
+  time.sleep(.1)
+  right()
+  time.sleep(2)
+
+def turn_left():
+  enc_tgt(0,1,15)
+  time.sleep(.1)
+  left()
+  time.sleep(2)
+
+
+move_forward(4)
+turn_right()
+move_forward(3)
+turn_right()
+move_forward(2)
+turn_left()
+move_forward(1)
+turn_left()
+move_forward(3)
